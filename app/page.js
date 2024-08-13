072702171bb7f455b7e9317840a0cb6079b6e508
Galtbatzana <galt.batzana1@gmail.com>
2024-08-13 20:47:32 +0800

    edit delete

--- app/page.js
+++ app/page.js
@@ -5,24 +5,66 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [articles, setArticles] = useState([]);
+  // const [articles, setArticles] = useState([]);
 
-  useEffect(()=> {
-    fetch("http://localhost:4000/articles")
+  // useEffect(()=> {
+  //   fetch("http://localhost:4000/articles")
+  //   .then((res)=>res.json())
+  //   .then((data)=>{
+  //     setArticles(data);
+  //   });
+  // }, []); 
+
+
+  // return (
+  //   <main>
+  //     {
+  //       articles.map((article)=>(
+  //         <div key={article.id}>{article.title}</div>
+  //       ))
+  //     }
+  //   </main>
+  // );
+////////////////////////////////////////////////////////
+  const [categories, setCategories] = useState([]);
+  
+
+  //automataar render hiij bga function loadList geed ner ugchie
+  function loadList() {
+    fetch("http://localhost:4000/categories/list")
     .then((res)=>res.json())
     .then((data)=>{
-      setArticles(data);
+      setCategories(data);
     });
-  }, []); 
+  }
 
+  useEffect(()=> {
+   loadList();
+  }, []);
+
+  function createNew(){
+    const name = prompt("Name...");
+    
+    fetch(`http://localhost:4000/categories/create?name=${name}`)
+    .then((res)=>res.json())
+    .then(()=>{
+      loadList();
+    });
+  }
+//ingeed uuruu backend ruu hussen data-gaa yawuulj chadaj bn
 
   return (
     <main>
-      {
-        articles.map((article)=>(
-          <div key={article.id}>{article.title}</div>
-        ))
-      }
+      <button onClick={createNew}>Add new</button>
+      {categories.map((category)=>(
+          <div key={category.name}>{category.name}
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        ))}
     </main>
   );
+
 }
+
+
