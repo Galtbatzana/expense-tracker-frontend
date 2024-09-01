6014df9e1ace9f1b31c1ea7ea416774c0bc298c3
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-09-01 14:48:10 +0800

    zasaw surguuli deer

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Slider } from "@radix-ui/react-slider";
-import { Hero } from "@/components/Hero";
+
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
@@ -99,7 +99,190 @@ export default function Home() {
   return (
     <main className="container mx-auto bg-[#F3F4F6] max-w-[1440px]">
       <Header />
-      <Hero />
+
+      <div className="container mx-auto flex bg-[#E5E7EB]">
+        <div className="w-[282px] mx-4 bg-white">
+          <div className="mx-3">
+            <div className="mb-3">
+              <p className="font-bold mb-3 mt-6">Records</p>
+              <Button
+                variant="secondary"
+                className="bg-[#0166FF] text-white rounded-full px-4 w-full"
+                onClick={() => setOpen(true)}
+              >
+                + Add Category
+              </Button>
+              <Dialog open={open}>
+                <DialogTrigger asChild>
+                  {/* <Button variant="outline">Edit Profile</Button> */}
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Categories</DialogTitle>
+                  </DialogHeader>
+                  <div className="flex gap-4">
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="secondary">
+                          <House />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-80">
+                        <div className="grid grid-cols-6 gap-2">
+                          {catergoriesIcons.map(({ name, Icon }) => (
+                            <div
+                              key={name}
+                              onClick={() => setIcon(name)}
+                              className={`flex items-center justify-center ${
+                                icon === name ? "bg-blue-500" : ""
+                              }`}
+                            >
+                              <Icon />
+                            </div>
+                          ))}
+                        </div>
+
+                        <div className="flex gap-2 m-2">
+                          {categoriesColors.map(({ name, value }) => (
+                            <div
+                              key={name}
+                              onClick={() => setColor(name)}
+                              className="w-8 h-8 rounded-full text-white flex items-center justify-center"
+                              style={{ backgroundColor: value }}
+                            >
+                              {color === name && <Check className="w-4 h-4" />}
+                            </div>
+                          ))}
+                        </div>
+
+                      </PopoverContent>
+                    </Popover>
+                    <Input
+                      value={name}
+                      onChange={(event) => setName(event.target.value)}
+                    />
+                  </div>
+                  <DialogFooter>
+                    <Button
+                      onClick={() => setOpen(false)}
+                      type="submit"
+                      className="w-full rounded-full bg-green-600 hover:bg-green-900"
+                    >
+                      Add
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-full max-w-xs rounded-lg p-2 mb-3 bg-[#E5E7EB]"
+            />
+            <div className="flex-col my-4">
+              <p className="font-bold my-6">Types</p>
+              <div className="flex items-center gap-2 mb-3">
+                <Checkbox className="rounded-full" />
+                <p>ALL</p>
+              </div>
+              <div className="flex items-center gap-2 mb-3">
+                <Checkbox className="rounded-full" />
+                <p>Income</p>
+              </div>
+              <div className="flex items-center gap-2 mb-3">
+                <Checkbox className="rounded-full" />
+                <p>Expense</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-col mx-3">
+            <div className="flex justify-between py-6">
+              <p className="font-bold">Category</p>
+              <button className="text-[#1F2937]">Clear</button>
+            </div>
+
+            <div>
+              <div>+ Add Category</div>
+            </div>
+          </div>
+          <div className="mx-3">
+            <p>Amount Range</p>
+            <div className="flex gap-2 mt-3">
+              <Input className="bg-[#E5E7EB]" placeholder="0" />
+              <Input className="bg-[#E5E7EB]" placeholder="100" />
+            </div>
+            <div className="my-4">
+              <Slider />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 mx-4">
+          <div className="mt-6">
+            <div>
+              <Carousel /> last 30 days
+            </div>
+            <div>
+              <Input /> Newest fisrt
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-col bg-[#E5E7EB] gap-3">
+              <div className="flex items-center justify-between bg-white rounded-lg">
+                <div className="flex items-center justify-center py-3 pl-6">
+                  <Checkbox className="mr-2" />
+                  <p>Select all</p>
+                </div>
+                <p className="pr-6">35.500$</p>
+              </div>
+              <p>Today</p>
+              <div className="flex items-center justify-between bg-white rounded-lg">
+                <div className="flex items-center py-3 pl-6">
+                  <Checkbox className="mr-2" />
+                  <div className="m-2 w-8 h-8 rounded-full">
+                    <House />
+                  </div>
+                  <div>
+                    <p>Lending & Renting</p>
+                    <p className="text-[12px]">14:00</p>
+                  </div>
+                </div>
+                <p className="pr-6">1.000$</p>
+              </div>
+              <div className="flex items-center justify-between bg-white rounded-lg">
+                <div className="flex items-center py-3 pl-6">
+                  <Checkbox className="mr-2" />
+                  <div className="m-2 w-8 h-8 rounded-full">
+                    <House />
+                  </div>
+                  <div>
+                    <p>Food & Drinks</p>
+                    <p className="text-[12px]">14:00</p>
+                  </div>
+                </div>
+                <p className="mr-6">1.000$</p>
+              </div>
+              <p>Yesterday</p>
+              <div className="flex items-center justify-between bg-white rounded-lg">
+                <div className="flex items-center py-3 pl-6">
+                  <Checkbox className="mr-2" />
+                  <div className="m-2 w-8 h-8 rounded-full">
+                    <House />
+                  </div>
+                  <div>
+                    <p>Lending & Renting</p>
+                    <p className="text-[12px]">14:00</p>
+                  </div>
+                </div>
+                <p className="mr-6">1.000$</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div>
         <Button onClick={createNew}>Add new</Button>
